41a83139fd970eaacb8370cc3e875e49427c313c
Yann DEGAT <yann.degat@gmail.com>
2018-04-16 11:59:46 +0200

    Add openstack func image_catalog_url

--- scripts/openstack_functions.sh
+++ scripts/openstack_functions.sh
@@ -14,9 +14,25 @@ function auth(){
     echo $RESP | awk -v RS='\r' '/X-Subject-Token: /{print $2}'
 }
 
+function image_catalog_url(){
+    cat $CATALOG | jq -r '.[]|select(.type == "image")|.endpoints[]|select(.interface== "public" and .region == "'$OS_REGION_NAME'").url'
+}
+
 function image_url(){
-    image_base_url=$(cat $CATALOG | jq -r '.[]|select(.type == "image")|.endpoints[]|select(.interface== "public" and .region == "'$OS_REGION_NAME'").url')
-    echo ${image_base_url}/$(curl -qs -H "X-Auth-Token: ${OS_TOKEN}" "${image_base_url}/v2/images" | jq -r '.images|sort_by(.created_at)|.[]|select(.name == "'"$IMAGE_NAME"'" and .image_state == "available")|.file' | tail -1)
+    local name=$1
+    local version=$2
+    local version_selector=""
+    if [ ! -z "$version" ]; then
+        version_selector='and .version == "'$version'"'
+    fi
+
+    image_catalog_url=$(cat $CATALOG | jq -r '.[]|select(.type == "image")|.endpoints[]|select(.interface== "public" and .region == "'$OS_REGION_NAME'").url')
+    uri="$(curl -qs -H "X-Auth-Token: ${OS_TOKEN}" "${image_catalog_url}/v2/images" | jq -r '.images|sort_by(.created_at)|.[]|select(.name == "'"$name"'" and .image_state == "available" '$version_selector')|.file' | tail -1)"
+    if [ ! -z "$uri" ]; then
+        echo ${image_catalog_url}${uri}
+    else
+        return 1
+    fi
 }
 
 function network_id(){
