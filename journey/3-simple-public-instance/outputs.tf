925f50b4b227a156fc2fa2ca004ed9eb5f1f6fd2
Yann DEGAT <yann.degat@gmail.com>
2018-06-21 15:12:37 +0200

    Rework for blog

--- journey/3-simple-public-instance/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "helper" {
-  description = "human friendly helper"
-  value = <<DESC
-You can now connect to your instance:
-
- $ ssh centos@${openstack_compute_instance_v2.instance.access_ip_v4}
-DESC
-}
--- journey/5-private-instances/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "helper" {
-  description = "human friendly helper"
-  value = <<DESC
-You can now connect to your instances:
-   ${indent(3, join("\n", formatlist("$ ssh -J centos@%s centos@%s", openstack_compute_instance_v2.bastion.access_ip_v4, openstack_compute_instance_v2.instances.*.access_ip_v4)))}
-DESC
-}
